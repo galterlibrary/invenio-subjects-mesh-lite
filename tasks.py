1a277ed437f5e8ddce508dbbe798af8cdfca1bbb
Guillaume Viger <fenekku@fenekku.com>
2023-11-02 14:07:03 -0400

    :tada: initial commit

---
+++ tasks.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2023 Northwestern University.
+#
+# invenio-subjects-mesh-lite is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+from invoke import task
+
+
+@task
+def test(c, color=True, passthru=""):
+    """Run tests."""
+    c.run(f"python -m pytest {passthru}", pty=color)
+
+
+@task
+def check_manifest(c, passthru=""):
+    """Check manifest."""
+    c.run(f"python -m check_manifest --no-build-isolation {passthru}")
